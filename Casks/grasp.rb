9b4f9909d0755c527e968d6bf392fc77673c95b8
elmeeee <37185272+elmeeee@users.noreply.github.com>
2026-08-11 16:45:00 +0700

    fix(cask): update download url asset filename to Grasp-macOS.zip

--- Casks/grasp.rb
+++ Casks/grasp.rb
@@ -2,7 +2,7 @@ cask "grasp" do
   version "1.0.0"
   sha256 :no_check
 
-  url "https://github.com/elmeeee/Grasp-drop/releases/download/v#{version}/Grasp.app.zip"
+  url "https://github.com/elmeeee/Grasp-drop/releases/download/v#{version}/Grasp-macOS.zip"
   name "Grasp"
   desc "macOS Quick Share Receiver & Cross-Platform File Hub"
   homepage "https://github.com/elmeeee/Grasp-drop"
